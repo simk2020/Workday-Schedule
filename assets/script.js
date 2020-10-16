9c9e8903a4886bb3eeb5a6bad4380f71209b601c
Sim kaur <moblue2012@gmail.com>
2020-10-16 01:26:54 -0400

    1st psuh

---
+++ assets/script.js
@@ -0,0 +1,25 @@
+
+var saveBtn = document.querySelector(".SaveBtn")
+$(document).ready(function(){
+    var time = moment().format('LLLL');
+$("#time").text(time);
+});
+
+
+// var save = document.querySelector("#saveBtn")
+// ("SaveBtn").textContent = ""
+// unction Save  
+
+// saveBtn.addEventListener("click", function(event) {
+//   event.preventDefault();
+
+// save.addEventListener("click", function(event) {
+//     event.preventDefault();
+//     textArea.save = localStorage;
+//     elements.forEach(function(element) {
+//       element.textContent = "";
+//     });
+//   })
+//     var text = document.querySelector("").value;
+//       localStorage.setItem("text",save );
+//     })
