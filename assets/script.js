0a016a66db23a9bc9b1efe3ea3aa1c5e00703efd
Sim kaur <moblue2012@gmail.com>
2020-10-17 14:38:36 -0400

    update

--- assets/script.js
+++ assets/script.js
@@ -5,21 +5,26 @@ $(document).ready(function(){
 $("#currentDay").text(time);
 });
 
-var timeArray = ["9am","10am","11am","Noon", "1pm","2pm","3pm", "4pm", "5pm"];
-for (var i=0; i>timeArray.length[i]; i++);
-console.log (timeArray);
-$("#timeblocks").append(timeArray);
-$(".container").append (timeArray);
+$(document).ready(function(){
+$('.saveBtn').on ('click',function (){
+var textAreaContent = $(this).siblings('textarea').val();
+console.log (textAreaContent);
+});
+});
+
+// var timeArray = ["9am","10am","11am","Noon", "1pm","2pm","3pm", "4pm", "5pm"];
+// for (var i=0; i>timeArray.length[i]; i++);
+// console.log (timeArray);
+// $("#timeblocks").append(timeArray);
+// $(".container").append (timeArray);
 
 var save = ("<button>");
 $("#save").append("");
 
+// $save.addEventListener("click", function(event) {
+// event.preventDefault();
 
-$save.addEventListener("click", function(event) {
-event.preventDefault();
-textArea.save = localStorage;
-console.log (textArea.save)
-});
 
-var text = document.querySelector("").value;
-localStorage.setItem("text",save )
+var text = localStorage.getItem(".maintext" );
+console.log(localStorage);
+console.log (text);
