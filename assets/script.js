d4a0e98d9881f4c3bf5035f5fa5523983c837d86
Sim kaur <moblue2012@gmail.com>
2020-10-17 14:57:46 -0400

    html done

--- assets/script.js
+++ assets/script.js
@@ -5,26 +5,13 @@ $(document).ready(function(){
 $("#currentDay").text(time);
 });
 
-$(document).ready(function(){
-$('.saveBtn').on ('click',function (){
-var textAreaContent = $(this).siblings('textarea').val();
-console.log (textAreaContent);
-});
-});
-
-// var timeArray = ["9am","10am","11am","Noon", "1pm","2pm","3pm", "4pm", "5pm"];
-// for (var i=0; i>timeArray.length[i]; i++);
-// console.log (timeArray);
-// $("#timeblocks").append(timeArray);
-// $(".container").append (timeArray);
-
-var save = ("<button>");
-$("#save").append("");
-
-// $save.addEventListener("click", function(event) {
-// event.preventDefault();
 
 
-var text = localStorage.getItem(".maintext" );
-console.log(localStorage);
-console.log (text);
+
+
+// $(document).ready(function(){
+// $('.saveBtn').on ('click',function (){
+// var textAreaContent = $(this).siblings('textarea').val();
+// console.log (textAreaContent);
+// });
+// });
